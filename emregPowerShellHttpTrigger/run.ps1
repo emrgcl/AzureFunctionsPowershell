41b702ac9e13fd147e277725dcca51b5cfefd4ab
Emre Güçlü <emreg@microsoft.com>
2023-01-05 10:29:51 +0300

    updated

--- emregPowerShellHttpTrigger/run.ps1
+++ emregPowerShellHttpTrigger/run.ps1
@@ -5,23 +5,23 @@ param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
-
+$SleepMilliseconds = get-random -Minimum 100 -Maximum 2000
 # Interact with query parameters or the body of the request.
 $name = $Request.Query.Name
 if (-not $name) {
     $name = $Request.Body.Name
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. MSI_SECRET:$($env:MSI_SECRET)"
+$body = "This HTTP triggered function executed successfully. Sleep Ms= '$SleepMilliseconds' Pass a name in the query string or in the request body for a personalized response. MSI_SECRET:$($env:MSI_SECRET)"
 
 Write-Verbose "MSI_SECRET:$($env:MSI_SECRET)"
 if ($name) {
     $Context = Get-AzContext
     Write-Host $Context
-    $body = "Hello, $name. This HTTP triggered function executed successfully. MSI_SECRET:$($env:MSI_SECRET), Subcription: $($Context.Subscription.Name), TenantID: $($Context.Tenant.Id) "
+    $body = "Hello, $name. Sleep Ms= '$SleepMilliseconds' This HTTP triggered function executed successfully. MSI_SECRET:$($env:MSI_SECRET), Subcription: $($Context.Subscription.Name), TenantID: $($Context.Tenant.Id) "
     
 }
-Start-Sleep -Milliseconds (get-random -Minimum 100 -Maximum 2000)
+Start-Sleep -Milliseconds $SleepMilliseconds
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
